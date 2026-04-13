0271425deb67465693bb527bdc71dee95b92e12a
RissN <farisazizishie@gmail.com>
2026-04-13 10:26:32 +0700

    fix: rewrite localhost image URLs to ngrok URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -20,4 +20,42 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+/**
+ * Response interceptor: Rewrite localhost URLs to API_BASE (ngrok) URL.
+ * This fixes image/media URLs stored in the database as http://127.0.0.1:8000/storage/...
+ * so they become accessible from the internet via ngrok.
+ */
+if (API_BASE) {
+  const LOCAL_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+  ];
+
+  const rewriteUrls = (data: any): any => {
+    if (typeof data === 'string') {
+      let result = data;
+      for (const origin of LOCAL_ORIGINS) {
+        result = result.replaceAll(origin, API_BASE);
+      }
+      return result;
+    }
+    if (Array.isArray(data)) {
+      return data.map(rewriteUrls);
+    }
+    if (data && typeof data === 'object') {
+      const result: any = {};
+      for (const key of Object.keys(data)) {
+        result[key] = rewriteUrls(data[key]);
+      }
+      return result;
+    }
+    return data;
+  };
+
+  api.interceptors.response.use((response) => {
+    response.data = rewriteUrls(response.data);
+    return response;
+  });
+}
+
 export default api;
